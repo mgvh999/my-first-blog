9ce319d9a803a8d9888d3caf9e94675bd94d4677
Max <mvhost2@gmail.com>
2017-02-02 14:02:24 -0300

    First Template, post_list.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,10 @@ from mysite.views import hello, current_datetime,hours_ahead
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$', hello),
-    url(r'^time/$', current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'', include('blog.urls')),
+    #url(r'^hello/$', hello),
+    #url(r'^time/$', current_datetime),
+    #url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+
+
 ]
