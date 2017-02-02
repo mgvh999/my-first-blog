061450bd4678726004c850f11e06697406d8d30f
Max <mvhost2@gmail.com>
2017-02-02 14:10:55 -0300

    Fixed import mistake on mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from mysite.views import hello, current_datetime,hours_ahead
+#from mysite.views import hello, current_datetime,hours_ahead
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
