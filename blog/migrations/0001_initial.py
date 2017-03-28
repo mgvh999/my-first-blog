d5288b1f9cf829e32a9c69342417cc18eec4cc58
Max <mvhost2@gmail.com>
2017-03-28 12:49:10 -0300

    troubleshooting

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
 import django.utils.timezone
+import cloudinary.models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -16,11 +17,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
-                ('title', models.CharField(max_length=200)),
-                ('text', models.TextField()),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(blank=True, null=True, max_length=200)),
+                ('text', models.TextField(blank=True, null=True)),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('published_date', models.DateTimeField(null=True, blank=True)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('url', models.URLField(blank=True, null=True)),
+                ('image', cloudinary.models.CloudinaryField(blank=True, null=True, verbose_name='image', max_length=255)),
+                ('video_url', models.URLField(blank=True, null=True)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
